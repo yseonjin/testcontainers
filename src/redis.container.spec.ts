ff29d4693fdddd386e8447497bcf9faffb7c94d2
sj.yoon <sj.yoon@toss.im>
2024-08-07 22:00:24 +0900

    feat: test 파일 작성

---
+++ src/redis.container.spec.ts
@@ -0,0 +1,61 @@
+import { RedisContainer, StartedRedisContainer } from '@testcontainers/redis';
+import Redis, { Redis as RedisClientType } from 'ioredis';
+
+// Jest 설정
+jest.setTimeout(60000);
+
+describe('Redis Test with Testcontainers', () => {
+  let redisClient: RedisClientType;
+  let container: StartedRedisContainer;
+
+  beforeAll(async () => {
+    // Redis 컨테이너 시작
+    container = await new RedisContainer().start();
+
+    // Redis 클라이언트 생성 및 연결
+    redisClient = new Redis(container.getConnectionUrl());
+  });
+
+  afterAll(async () => {
+    // Redis 클라이언트 연결 종료 및 컨테이너 중지
+    await redisClient.quit();
+    await container.stop();
+  });
+
+  beforeEach(async () => {
+    await redisClient.flushall(); // 모든 키 삭제
+  });
+
+  test('Redis에 값을 설정하고 가져오기', async () => {
+    // Redis에 값 설정
+    await redisClient.set('key', 'value');
+
+    // Redis에서 값 가져오기
+    const value = await redisClient.get('key');
+
+    // 값 검증
+    expect(value).toBe('value');
+  });
+
+  test('존재하지 않는 키 처리', async () => {
+    // 존재하지 않는 키 가져오기
+    const value = await redisClient.get('non-existing-key');
+
+    // 값 검증
+    expect(value).toBeNull();
+  });
+
+  test('connected_clients 확인해보기', async () => {
+    const queryResult = await container.executeCliCmd('info', ['clients']);
+    const connectedClientsMatch = queryResult.match(/connected_clients:(\d+)/);
+
+    if (connectedClientsMatch) {
+      const connectedClients = parseInt(connectedClientsMatch[1], 10);
+      console.log(`Connected Clients: ${connectedClients}`);
+
+      expect(connectedClients).toBeGreaterThanOrEqual(1);
+    } else {
+      console.error('Unable to retrieve connected_clients info');
+    }
+  });
+});
---
+++ src/redis.generic-container.spec.ts
@@ -0,0 +1,60 @@
+import { GenericContainer, StartedTestContainer } from 'testcontainers';
+import Redis from 'ioredis';
+
+jest.setTimeout(1000000);
+describe('redis generic-container test', () => {
+  let container: StartedTestContainer;
+  let redisClient: Redis;
+
+  beforeAll(async () => {
+    // Redis 컨테이너 생성 및 시작
+    container = await new GenericContainer('redis')
+      .withExposedPorts(6379)
+      .start();
+
+    const port = container.getMappedPort(6379);
+    const host = container.getHost();
+
+    // Redis 클라이언트 초기화
+    redisClient = new Redis({
+      host,
+      port,
+    });
+
+    // 클라이언트가 이미 연결되어 있거나 연결 중인지 확인
+    // RedisStatus = "wait" | "reconnecting" | "connecting" | "connect" | "ready" | "close" | "end";
+    if (redisClient.status !== 'connecting') {
+      await redisClient.connect();
+    }
+  });
+
+  // beforeEach(async () => {
+  //   // Redis 초기화 (필요에 따라 수행)
+  //   await redisClient.flushall(); // 모든 키 삭제
+  // });
+
+  afterAll(async () => {
+    // Redis 클라이언트 연결 종료 및 컨테이너 중지
+    await redisClient.quit();
+    await container.stop();
+  });
+
+  test('Redis에 값을 설정하고 가져오기', async () => {
+    // Redis에 값 설정
+    await redisClient.set('key', 'value');
+
+    // Redis에서 값 가져오기
+    const value = await redisClient.get('key');
+
+    // 가져온 값이 예상 값인지 확인
+    expect(value).toBe('value');
+  });
+
+  test('존재하지 않는 키 처리', async () => {
+    // 존재하지 않는 키 가져오기
+    const value = await redisClient.get('non-existing-key');
+
+    // 가져온 값이 null인지 확인 (ioredis는 존재하지 않는 키에 대해 null을 반환함)
+    expect(value).toBe(null);
+  });
+});
